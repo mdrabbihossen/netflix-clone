28fe22a5afbcefb612e7f3f4f753e6a39b64e625
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2022-04-21 17:23:34 +0600

    netflix clone

---
+++ lib/pages/root_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RootApp extends StatelessWidget {
+  const RootApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
