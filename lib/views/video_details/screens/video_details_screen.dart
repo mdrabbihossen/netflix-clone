4f5763bcf2918433c0efac9448630558119743d9
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2022-10-26 03:32:15 +0600

    adding video player controller

--- lib/views/video_details/screens/video_details_screen.dart
+++ lib/views/video_details/screens/video_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants/custom_appbar.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoDetailScreen extends StatefulWidget {
   final String videoUrl;
@@ -13,8 +15,52 @@ class VideoDetailScreen extends StatefulWidget {
 }
 
 class _VideoDetailScreenState extends State<VideoDetailScreen> {
+  VideoPlayerController? controller;
+
+  @override
+  void initState() {
+    // implement initState
+    super.initState();
+    controller = VideoPlayerController.asset(widget.videoUrl)
+      ..initialize().then((value) {
+        setState(() {
+          controller?.play();
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller?.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: customAppBar(
+        color: Colors.black,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+          ),
+        ),
+        appBarTitle: '',
+      ),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
   }
 }
